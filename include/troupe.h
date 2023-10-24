44f3254f5c37a44e61252e4046933133f9529fd8
regna <45128415+nregna@users.noreply.github.com>
2023-10-24 23:03:27 +0200

    Update troupe.h

--- include/troupe.h
+++ include/troupe.h
@@ -12,6 +12,8 @@ class pierrot{
         Draw draw; Ballot ballot;
         int const haste = 10;
         bool update(SDL_Event&);
+        bool init(SDL_Window*);
+        bool close();
     private:
         int* xpos=0;
         int* ypos=0;
@@ -23,9 +25,19 @@ class pierrot{
 
 bool pierrot::update(SDL_Event& arg){
     ballot.handle(arg, *xpos, *ypos, xvel, yvel, haste);
-    chuyen(xvel, xpos);
-    chuyen(yvel, ypos);
-    draw.sprite(xpos, ypos);
+    chuyen(xvel, xpos, 391);
+    chuyen(yvel, ypos, 293);
+    draw.sprite(xpos, ypos, ten, rei);
+};
+
+bool pierrot::init(SDL_Window* wind){
+    rei=SDL_CreateRenderer(wind, -1, SDL_RENDERER_ACCELERATED); SDL_SetRenderDrawColor(rei, 0xFF, 0xFF, 0xFF, 0xFF);
+    draw.texture(ten, rei, "swan.png");
+};
+
+bool pierrot::close(){
+    dest(ten);
+    SDL_DestroyRenderer(rei); rei=NULL;
 };
 
 #endif // TROUPE_H_INCLUDED
