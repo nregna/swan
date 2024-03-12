dc0656a24d2531df4fae117670c47dc653015d5b
regna <45128415+nregna@users.noreply.github.com>
2024-03-12 19:19:13 +0100

    Update troupe.h

--- include/troupe.h
+++ include/troupe.h
@@ -7,37 +7,38 @@
 #include"mekanikk.h"
 #include"poll.h"
 
-class pierrot{
+class Pierrot{
     public:
         Draw draw; Ballot ballot;
-        int const haste = 10;
-        bool update(SDL_Event&);
-        bool init(SDL_Window*);
-        bool close();
+        bool update(SDL_Event&, SDL_Renderer*), init(SDL_Renderer*), close(), texture();
     private:
-        int* xpos=0;
-        int* ypos=0;
-        int xvel=0, yvel=0;
+        int const haste = 10;
+        int xpos=0, ypos=0, xvel=0, yvel=0;
     protected:
         SDL_Texture* ten=NULL;
-        SDL_Renderer* rei=NULL;
+//        SDL_Renderer* rei=NULL;
 };
 
-bool pierrot::update(SDL_Event& arg){
-    ballot.handle(arg, *xpos, *ypos, xvel, yvel, haste);
-    chuyen(xvel, xpos, 391);
-    chuyen(yvel, ypos, 293);
+bool Pierrot::update(SDL_Event& arg, SDL_Renderer* rei){
+    ballot.handle(arg, &xvel, &yvel, haste);
+    chuyen(&xvel, &xpos, 391);
+    chuyen(&yvel, &ypos, 293);
+    draw.sprite(xpos, ypos, ten, rei);
+    printf("pos: %d - %d\n\n",xpos,ypos);
+};
+
+bool Pierrot::init(SDL_Renderer* rei){
+    draw.texture(ten, rei, "swan.png");
     draw.sprite(xpos, ypos, ten, rei);
 };
 
-bool pierrot::init(SDL_Window* wind){
-    rei=SDL_CreateRenderer(wind, -1, SDL_RENDERER_ACCELERATED); SDL_SetRenderDrawColor(rei, 0xFF, 0xFF, 0xFF, 0xFF);
-    draw.texture(ten, rei, "swan.png");
+bool Pierrot::close(){
+    dest(ten);
+//    SDL_DestroyRenderer(rei); rei=NULL;
 };
 
-bool pierrot::close(){
-    dest(ten);
-    SDL_DestroyRenderer(rei); rei=NULL;
+bool Pierrot::texture(){
+    return ten;
 };
 
 #endif // TROUPE_H_INCLUDED
