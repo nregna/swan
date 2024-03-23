000c08b81314d54dac302f9b804542ce2e03fda1
regna <45128415+nregna@users.noreply.github.com>
2024-03-23 19:40:47 +0100

    Update mekanikk.h

--- include/mekanikk.h
+++ include/mekanikk.h
@@ -1,20 +1,16 @@
-#ifndef AUTOMATA_H_INCLUDED
-#define AUTOMATA_H_INCLUDED
+#ifndef MEKANIKK_H_INCLUDED
+#define MEKANIKK_H_INCLUDED
 
 #include<iostream>
 
 bool chuyen(int* vel, int* pos, int s){
-    if(*pos==0||*pos==s){
-        (*vel)=-2*(*vel);
-    }else if(*pos+(*vel)>s){
-        (*vel)=s-*pos;
-    }else if(*pos-(*vel)<0){
-        (*vel)=0-*pos;
-    }else{
-        return false;
-    };
-    *pos+=(*vel);
+	if(*pos<=0){
+		*pos=s;
+	}else if(*pos>=s){
+		*pos=0;
+	};
+	*pos-=(*vel);
 	return true;
 };
 
-#endif // AUTOMATA_H_INCLUDED
+#endif // MEKANIKK_H_INCLUDED
